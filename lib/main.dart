1608f0202d7b6e766ba1afa81e33b0e853176b75
joeolamide <odulatejoseph@gmail.com>
2022-05-19 13:04:20 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/categories_screen.dart';
+
+void main () => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CategoriesScreen(),
+    );
+  }
+}
+
+
